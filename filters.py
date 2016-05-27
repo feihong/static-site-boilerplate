4d0e17e75bcd9cf05f21ad6782f3b46ac5210858
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-26 20:30:57 -0500

    Set the initial header level of ReST documents to 2

--- filters.py
+++ filters.py
@@ -3,7 +3,10 @@ from docutils.core import publish_parts
 
 
 def rst(s):
-    result = publish_parts(s, writer_name="html")["html_body"]
+    result = publish_parts(
+        s,
+        writer_name='html',
+        settings_overrides={'initial_header_level': 2})['html_body']
     # Get rid of the outer div.
     return result[22:-7]
 
